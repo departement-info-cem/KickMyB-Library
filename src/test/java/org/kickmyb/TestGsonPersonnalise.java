b22db3215f77600b9de1029a06721b5c6fb048d2
Joris Deguet <joris.deguet@gmail.com>
2025-05-10 11:46:45 -0400

    Modif aux tests

--- src/test/java/org/kickmyb/TestGsonPersonnalise.java
+++ src/test/java/org/kickmyb/TestGsonPersonnalise.java
@@ -9,12 +9,12 @@ public class TestGsonPersonnalise {
 
     @Test
     public void testDate(){
-        RequeteAjoutTache atr = new RequeteAjoutTache();
-        atr.dateLimite = new Date();
-        atr.nom = "gna gna";
-        String s = GsonPersonnalise.gsonPerso().toJson(atr);
-
+        RequeteAjoutTache rat = new RequeteAjoutTache();
+        rat.dateLimite = new Date();
+        rat.nom = "gna gna";
+        String s = GsonPersonnalise.gsonPerso().toJson(rat);
+        System.out.println(rat.nom + " " + rat.dateLimite);
         RequeteAjoutTache recov = GsonPersonnalise.gsonPerso().fromJson(s, RequeteAjoutTache.class);
-        System.out.println(recov);
+        System.out.println(recov.nom + " " + recov.dateLimite);
     }
 }
