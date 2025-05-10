b527a13bdc4ef88e538c57307983ec8f66b8ebf3
Joris Deguet <joris.deguet@gmail.com>
2025-05-10 11:44:06 -0400

    Modification pour inscription

--- src/main/java/org/kickmyb/transfer/RequeteInscription.java
+++ src/main/java/org/kickmyb/transfer/RequeteInscription.java
@@ -1,7 +1,7 @@
 package org.kickmyb.transfer;
 
-public class RequeteInscription {
+public class RequeteInscription extends RequeteConnexion {
 
-    public String nom;
-    public String motDePasse;
+    public String confirmationMotDePasse; // confirmation du mot de passe
+
 }
